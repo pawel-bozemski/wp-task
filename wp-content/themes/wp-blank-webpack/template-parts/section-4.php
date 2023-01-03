0a9fe078352746e801ede14c815663715ccd6d6f
pawel-bozemski <paw.boz@gmail.com>
2023-01-03 13:55:14 +0100

    Add section 4

---
+++ wp-content/themes/wp-blank-webpack/template-parts/section-4.php
@@ -0,0 +1,48 @@
+<?php
+$fields = get_fields();
+$section = $fields['section_4'];
+?>
+<div class="container white">
+    <div class="wrapper">
+        <div class="section_4">
+            <?= $section['title'] ?>
+            <div class="section__list">
+                <div class="section__list__item">
+                    <img src="<?= $section['image_list_1'] ?>" alt="icon">
+                    <h3 class="h300"><?= $section['title_list_1'] ?></h3>
+                    <p><?= $section['subtitle_list_1'] ?></p>
+                </div>
+                <div class="section__list__item">
+                    <img src="<?= $section['image_list_2'] ?>" alt="icon">
+                    <h3 class="h300"><?= $section['title_list_2'] ?></h3>
+                    <p><?= $section['subtitle_list_2'] ?></p>
+                </div>
+                <div class="section__list__item">
+                    <img src="<?= $section['image_list_3'] ?>" alt="icon">
+                    <h3 class="h300"><?= $section['title_list_3'] ?></h3>
+                    <p><?= $section['subtitle_list_3'] ?></p>
+                </div>
+                <div class="section__list__item">
+                    <img src="<?= $section['image_list_4'] ?>" alt="icon">
+                    <h3 class="h300"><?= $section['title_list_4'] ?></h3>
+                    <p><?= $section['subtitle_list_4'] ?></p>
+                </div>
+                <div class="section__list__item">
+                    <img src="<?= $section['image_list_5'] ?>" alt="icon">
+                    <h3 class="h300"><?= $section['title_list_5'] ?></h3>
+                    <p><?= $section['subtitle_list_5'] ?></p>
+                </div>
+                <div class="section__list__item">
+                    <img src="<?= $section['image_list_6'] ?>" alt="icon">
+                    <h3 class="h300"><?= $section['title_list_6'] ?></h3>
+                    <p><?= $section['subtitle_list_6'] ?></p>
+                </div>
+                <div class="section__list__item">
+                    <img src="<?= $section['image_list_7'] ?>" alt="icon">
+                    <h3 class="h300"><?= $section['title_list_7'] ?></h3>
+                    <p><?= $section['subtitle_list_7'] ?></p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
